a798dfa5fbc1e5a7001ab763c136f12c9f452efd
Yu-ting0606 <hamy0606s@gmail.com>
2024-06-11 21:27:34 +0800

    revise_hier0611

--- R/Main_Function.R
+++ R/Main_Function.R
@@ -260,6 +260,178 @@ Stab_Syn_Multiple <- function(data, order.q=c(1,2), Alltime=TRUE, start_T=NULL, 
 }
 
 
+#' Calculate stability of the time series data for hierarchical structure.
+#'
+#' \code{Stab_Hier} is a function that calculate stability of the time series data (like biomass, productivity, etc.) for hierarchical structure.
+#'
+#' @param data can be input as \code{data.frame/matrix} (assemblages by times).
+#' @param mat hierarchical structure of data.
+#' @param order.q a numerical vector specifying the orders of stability. Default is c(1,2).
+#' @param Alltime \code{TRUE} or \code{FALSE}, to decide whether to use all the times in the data.
+#' @param start_T (argument only for \code{Alltime = FALSE}) a positive integer specifying the start column of time in the data.
+#' @param end_T (argument only for \code{Alltime = FALSE}) a positive integer specifying the end column of time in the data.
+#'
+#'
+#' @return a dataframe with columns "Hier", "Order_q", and stabiltiy "Gamma", "Alpha" and "Beta (normalized)".
+#'
+#' @examples
+#' library(dplyr)
+#'
+#' data("Jena_hierarchical_data")
+#' data("Jena_hierarchical_mat")
+#' Jena_hierarchical_data <- Jena_hierarchical_data[,-2]
+#' output <- Stab_Hier(data = Jena_hierarchical_data, mat = Jena_hierarchical_mat,
+#'                    q=c(1,2), Alltime=TRUE)
+#' output
+#'
+#' @export
+
+Stab_Hier <- function(data, mat, q=c(1,2), Alltime=TRUE, start_T=NULL, end_T=NULL){
+  if(Alltime==FALSE){
+    data <- data[,start_T:end_T]
+  }
+  TT <- ncol(data)
+
+  gamma_zz <- apply(data, 2, sum)
+  gamma <- sapply(q, function(qq){
+    if(qq==1){
+      gamma_zz <- gamma_zz[gamma_zz!=0]
+      gamma <- (-1)*sum((gamma_zz/sum(gamma_zz))*log((gamma_zz/sum(gamma_zz))))/log(TT)
+    }else{
+      gamma <- (1-sum((gamma_zz/sum(gamma_zz))^qq))/(1-TT^(1-qq))
+    }
+  })
+  gamma <- data.frame(hier=ncol(mat)+1, order_q=q, gamma_value=gamma)
+
+
+  hier_i_alphaanddiff <- function(data, mat, q, i){
+    TT <- ncol(data)
+    index <- unique(mat[,1:i])
+    if(i==1){
+      take <- sapply(1:length(index), function(rr)which(mat[,1:i]==index[rr]))
+    }else{
+      take <- sapply(1:nrow(index), function(rr)which(apply(mat[,1:i], 1, function(mm)sum(mm==index[rr,]))==i))
+    }
+    value <- lapply(take, function(tak){
+      zz <- data[tak, ]
+      zz <- apply(zz, 2, sum)
+      outout <- sapply(q, function(qq){
+        if(length(zz[zz!=0])!=0){
+          if(qq==1){
+            zz <- zz[zz!=0]
+            out <- (-1)*sum((zz/sum(zz))*log((zz/sum(zz))))/log(TT)
+          }else{
+            out <- (1-sum((zz/sum(zz))^qq))/(1-TT^(1-qq))
+          }
+          ww <- sum(zz)/sum(data)
+          vv <- ww*out
+        }else{
+          out <- 0
+          ww <- 0
+          vv <- ww*out
+        }
+        return(vv)
+      })
+      return(outout)
+    })
+
+    value <- do.call(rbind, value)
+    if(length(q)==1){
+      result <- sum(value)
+    }else{
+      result <- apply(value,2,sum)
+    }
+
+    ##diff
+    if(i>=2){
+      index2 <- unique(mat[,1:(i-1)])
+
+      if(i==2){
+        take2 <- sapply(1:length(index2), function(rr2){which(index[,1:(i-1)]==index2[rr2])})
+      }else{
+        take2 <- sapply(1:nrow(index2), function(rr2){which(apply(index[,1:(i-1)], 1, function(mm)sum(mm==index2[rr2,]))==(i-1))})
+      }
+
+      subdat <- lapply(take, function(tak){
+        zz <- data[tak, ]
+        zz <- apply(zz, 2, sum)
+        return(zz)
+      })
+
+      subdat2 <- lapply(take2, function(tak2){
+        zz <- subdat[tak2]
+        zz <- do.call(rbind, zz)
+        vv <- sapply(q, function(qq){
+          if(qq==1){
+            zz[which(zz==0)] <- 10^(-15)
+            value <- (-1)*sum((zz/sum(zz))*log(zz/sum(zz)))*(sum(zz)/sum(data))/log(TT)
+          }else{
+            value <- ((1-sum((zz/sum(zz))^qq))*(sum(zz)/sum(data)))/(1-TT^(1-qq))
+          }
+        })
+        return(vv)
+      })
+
+      subdat2 <- do.call(rbind, subdat2)
+      if(length(q)==1){
+        subdat2 <- sum(subdat2)
+      }else{
+        subdat2 <- apply(subdat2,2,sum)
+      }
+
+    }else{
+      subdat2 <- lapply(take, function(tak){
+        zz <- data[tak, ]
+        zz <- apply(zz, 2, sum)
+        vv <- sapply(q, function(qq){
+          if(length(zz[zz!=0])!=0){
+            if(qq==1){
+              zz <- zz[zz!=0]
+              out <- (-1)*sum((zz/sum(data))*log((zz/sum(data))))/log(TT)
+            }else{
+              out <- sum((zz/sum(data))^qq)
+            }
+          }else{
+            out <- 0
+          }
+          return(out)
+        })
+        return(vv)
+      })
+
+      subdat2 <- do.call(rbind, subdat2)
+      subdat2 <- sapply(1:length(q), function(ll){
+                    if(q[ll]==1){
+                      out <- sum(subdat2[,ll])
+                    }else{
+                      out <- (1-sum(subdat2[,ll]))/(1-TT^(1-q[ll]))
+                    }
+                  })
+    }
+    return(data.frame(hier=rep((ncol(mat)+1-i),length(q)), order_q=q, gamma_alpha=result, beta_max=subdat2-result))
+  }
+
+  stab <- c()
+  for(ii in 1:ncol(mat)){
+    stab <- rbind(stab, hier_i_alphaanddiff(data, mat, q, ii))
+  }
+
+  gamma_value <- rbind(gamma, gamma, data.frame(stab[,1:2], gamma_value=stab[,3]))
+  gamma_value <- filter(gamma_value, hier!=1)
+  gamma_value$hier <- rep(c((ncol(mat)+1):1), each=length(q))
+
+  alpha_value <- c(rep(NA,length(q)), stab[,3])
+  beta_max <- c(rep(NA,length(q)), stab[,4])
+  alldata <- cbind(gamma_value, alpha_value, beta_max)
+
+  alldata <- cbind(alldata[,1:4], beta_value=(alldata[,3]-alldata[,4])/alldata[,5])
+  alldata <- as.data.frame(alldata)
+  colnames(alldata) <- c("Hier","Order_q","Gamma","Alpha","Beta (normalized)")
+
+  return(alldata)
+}
+
+
 
 #' ggplot2 extension for a Stab_Single or Stab_Syn_Multiple object with q-profile.
 #'
